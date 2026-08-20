731c5aad4ac207a35a049fc69c94d0241ed81b4b
Sim-karta <simonenkonikita561@gmail.com>
2026-08-20 20:17:26 +0300

    Add filter array buttons

--- assets/scripts/set.js
+++ assets/scripts/set.js
@@ -1,8 +1,9 @@
+const setGroup = document.querySelector(".set");
+const outputArr1Btn = setGroup.querySelector(".output-arr1-btn");
+const outputArr2Btn = setGroup.querySelector(".output-arr2-btn");
+
 const arr1 = [1, 6, 9, 4, 9, 1, 5, 6];
 const arr2 = [5, 10, 11];
-
-console.log(filterArray(arr1));
-console.log(filterArray(arr1, arr2));
 
 function filterArray(arr) {
     if (arguments.length === 1) {
@@ -21,3 +22,11 @@ function filterArray(arr) {
 
     return null;
 }
+
+outputArr1Btn.addEventListener("click", () => {
+    console.dir(filterArray(arr1));
+});
+
+outputArr2Btn.addEventListener("click", () => {
+    console.dir(filterArray(arr1, arr2));
+});
