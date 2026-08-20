969659e888f3f7a794eeef3141ecd0c7b42de21a
Sim-karta <simonenkonikita561@gmail.com>
2026-08-20 18:27:14 +0300

    Make second array with unique elements

--- assets/scripts/set.js
+++ assets/scripts/set.js
@@ -2,7 +2,10 @@ const arr1 = [1, 6, 9, 4, 9, 1, 5, 6];
 const arr2 = [5, 10, 11];
 
 const set1 = new Set(arr1);
+const set2 = new Set(arr1, arr2);
 
 const newArr1 = [...set1];
+const newArr2 = [...set2];
 
 console.log(newArr1);
+console.log(newArr2);
