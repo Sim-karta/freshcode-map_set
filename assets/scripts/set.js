0991e83cb9a9566be857a778077a8346d50c521b
Sim-karta <simonenkonikita561@gmail.com>
2026-08-20 18:43:29 +0300

    Add function for filter array

--- assets/scripts/set.js
+++ assets/scripts/set.js
@@ -1,11 +1,23 @@
 const arr1 = [1, 6, 9, 4, 9, 1, 5, 6];
 const arr2 = [5, 10, 11];
 
-const set1 = new Set(arr1);
-const set2 = new Set(arr1, arr2);
+console.log(filterArray(arr1));
+console.log(filterArray(arr1, arr2));
 
-const newArr1 = [...set1];
-const newArr2 = [...set2];
+function filterArray(arr) {
+    if (arguments.length === 1) {
+        return [...new Set(arr)];
+    }
 
-console.log(newArr1);
-console.log(newArr2);
+    if (arguments.length > 1) {
+        const newSet = new Set();
+        Array.from(arguments).forEach((arr) => {
+            arr.forEach((item) => {
+                newSet.add(item);
+            });
+        });
+        return [...newSet];
+    }
+
+    return null;
+}
