87206e88713076d17234949db007961b7e9d7216
Sim-karta <simonenkonikita561@gmail.com>
2026-08-20 21:29:08 +0300

    Make map with cutomers info

---
+++ assets/scripts/orders.js
@@ -0,0 +1,27 @@
+const orders = [
+    { id: 101, customer: "Elena", category: "Electronics", amount: 1200 },
+    { id: 102, customer: "Ihor", category: "Clothing", amount: 300 },
+    { id: 103, customer: "Elena", category: "Books", amount: 150 },
+    { id: 104, customer: "Ihor", category: "Electronics", amount: 800 },
+    { id: 105, customer: "Elena", category: "Clothing", amount: 450 },
+    { id: 106, customer: "Maria", category: "Books", amount: 200 },
+];
+
+const customers = new Map();
+
+for (order of orders) {
+    if (!customers.has(order.customer)) {
+        customers.set(order.customer, {
+            totalAmount: order.amount,
+            ordersCount: 1,
+            categories: new Set().add(order.category),
+        });
+    } else {
+        const customer = customers.get(order.customer);
+        customer.totalAmount += order.amount;
+        customer.ordersCount++;
+        customer.categories.add(order.category);
+    }
+}
+
+console.dir(customers);
